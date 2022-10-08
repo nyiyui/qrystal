05a478c7facec2d3b226b0c34b96a25b7e4475be
Ken Shibata <inaba@nyiyui.ca>
2022-10-08 03:05:47 -0400

    debug signing

--- node/auth.go
+++ node/auth.go
@@ -4,6 +4,7 @@ import (
 	"crypto/ed25519"
 	"errors"
 	"fmt"
+	"log"
 
 	"github.com/nyiyui/qrystal/node/api"
 )
@@ -52,6 +53,8 @@ func (s *authState) solveChall() error {
 		copy(signThis, sq1.Chall)
 		signThis = append(signThis, added...)
 		challResp = ed25519.Sign(s.coordPrivKey, signThis)
+		log.Printf("signee: %x", signThis)
+		log.Printf("signature: %x", challResp)
 	}
 
 	sq2 := api.AuthS{
@@ -91,6 +94,9 @@ func (s *authState) verifyChall(cnn, yourName string) error {
 	signed := make([]byte, 64)
 	copy(signed, chall)
 	signed = append(signed, sq4.ChallAdded...)
+	log.Printf("your pubkey: %x", s.you.PublicKey)
+	log.Printf("signee: %x", signed)
+	log.Printf("signature: %x", sq4.ChallResp)
 	ok := ed25519.Verify(ed25519.PublicKey(s.you.PublicKey), signed, sq4.ChallResp)
 	if !ok {
 		return errors.New("signature verification failed")
