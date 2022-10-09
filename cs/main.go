c37562f11251c04e1192beae9b092c125b6af609
Ken Shibata <inaba@nyiyui.ca>
2022-10-09 19:31:22 -0400

    disallow nil ipnet

--- cs/main.go
+++ cs/main.go
@@ -104,7 +104,7 @@ func (s *CentralSource) rmChangeNotify(name string) {
 func FromIPNets(nets []net.IPNet) (dest []*api.IPNet) {
 	dest = make([]*api.IPNet, len(nets))
 	for i, n := range nets {
-		if n == nil {
+		if n.String() == "<nil>" {
 			panic("nil IPNet")
 		}
 		dest[i] = &api.IPNet{Cidr: n.String()}
