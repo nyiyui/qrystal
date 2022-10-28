51f83d25cb124afa74f594f99e00aaaea4d95afb
Ken Shibata <nyiyui@nyiyui.ca>
2022-10-27 23:06:45 -0400

    pkg-generic: add commit

--- pkg-generic-inside.sh
+++ pkg-generic-inside.sh
@@ -7,6 +7,8 @@ apt-get -qq install -y make
 
 prefix=/source
 
+current=$(cd /source && git rev-parse @)
+
 mkdir /build
 cd /build
 make -f $prefix/Makefile src=/source pkgdir=/build build2 cs-push
@@ -14,4 +16,5 @@ cp -r $prefix/config .
 mkdir ./mio
 cp $prefix/mio/dev-*.sh ./mio/
 cp $prefix/Makefile .
+echo $current > ./BUILD_COMMIT
 ls -al /build
