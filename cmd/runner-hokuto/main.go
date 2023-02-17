210241803984724123f93004d17c861e4276597b
nyiyui <+@nyiyui.ca>
2023-02-17 17:17:54 -0500

    hokuto: reset log level

--- cmd/runner-hokuto/main.go
+++ cmd/runner-hokuto/main.go
@@ -6,13 +6,11 @@ import (
 	"github.com/nyiyui/qrystal/hokuto"
 	"github.com/nyiyui/qrystal/profile"
 	"github.com/nyiyui/qrystal/util"
-	"go.uber.org/zap/zapcore"
 )
 
 func main() {
 	util.SetupLog()
 	defer util.S.Sync()
-	util.Atom.SetLevel(zapcore.InfoLevel)
 	log.SetPrefix("hokuto:  ")
 	log.SetFlags(log.LstdFlags | log.Lmsgprefix)
 	util.ShowCurrent()
