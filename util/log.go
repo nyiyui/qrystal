312571c1e8c4e0d189b4293952ffc3acd53be64f
nyiyui <+@nyiyui.ca>
2023-02-17 16:53:19 -0500

    hokuto: info level (util: dynamic log level changing)

--- util/log.go
+++ util/log.go
@@ -1,18 +1,27 @@
 package util
 
 import (
-	"log"
+	"os"
+	"sync"
 
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var L *zap.Logger
 var S *zap.SugaredLogger
+var Atom zap.AtomicLevel
+var setupLock sync.Mutex
 
 func SetupLog() {
-	L, err := zap.NewDevelopment()
-	if err != nil {
-		log.Fatalf("zap: %s", err)
-	}
+	setupLock.Lock()
+	defer setupLock.Unlock()
+	cfg := zap.NewDevelopmentConfig()
+	Atom = zap.NewAtomicLevel()
+	L := zap.New(zapcore.NewCore(
+		zapcore.NewConsoleEncoder(cfg.EncoderConfig),
+		zapcore.Lock(os.Stdout),
+		Atom,
+	))
 	S = L.Sugar()
 }
