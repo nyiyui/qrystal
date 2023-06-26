ba3615c3cf9291bbf4dcf47c6116521946964743
nyiyui <+@nyiyui.ca>
2023-06-26 15:46:05 -0400

    debug

--- node/azusa.go
+++ node/azusa.go
@@ -6,9 +6,11 @@ import (
 	"github.com/cenkalti/rpc2"
 	"github.com/nyiyui/qrystal/api"
 	"github.com/nyiyui/qrystal/central"
+	"github.com/nyiyui/qrystal/util"
 )
 
 func (n *Node) azusa(networks map[string]central.Peer, cl *rpc2.Client) (err error) {
+	util.S.Info("azusa: call: %#v", networks)
 	var s api.AzusaS
 	err = cl.Call("azusa", &api.AzusaQ{Networks: networks, CentralToken: n.cs.Token}, &s)
 	if err != nil {
